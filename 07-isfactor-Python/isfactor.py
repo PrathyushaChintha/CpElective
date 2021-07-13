b5681458b873f87d07a1d9769bc66d34933f6562
PrathyushaChintha <chintha.prathyusha01@msitprogram.net>
2021-07-14 00:47:51 +0530

    isfactor

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -6,4 +6,15 @@
 
 
 def fun_isfactor(f, n):
-	return False # replace with your solution
+	if(n==0):
+		return True
+	elif(f==0):  
+		return False
+
+	elif(n%f==0):
+		return True
+	elif(n%f!=0):
+		return False
+
+
+	
